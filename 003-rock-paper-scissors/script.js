6f34380c5ba4ca52828eebb81c679521b1f78720
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-03-15 12:43:42 +0100

    Player play, round play and game function

--- 003-rock-paper-scissors/script.js
+++ 003-rock-paper-scissors/script.js
@@ -4,17 +4,76 @@ function computerPlay(){
 
     switch(randomNumber){
         case 0:
-            play = "Rock";
+            play = "rock";
             break;
         case 1: 
-            play = "Paper";
+            play = "paper";
             break;
         case 2:
-            play = "Scissors";
+            play = "scissors";
             break;
     }
 
     return play;
 }
 
-console.log(computerPlay());
+function playerSelection(){
+    let userPlay = prompt("What is your play?").toLowerCase();
+
+    return userPlay;
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    let playerPoint = 0;
+    let computerPoint = 0;
+
+    if (playerSelection == computerSelection)
+    {
+        console.log(`You Tied! ${playerSelection} vs ${computerSelection}`);
+    }
+    else if(
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')
+    ){
+        playerPoint = 1;
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);        
+    }
+    else if(
+        (playerSelection == 'scissors' && computerSelection == 'rock') ||
+        (playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'scissors')
+    ){
+        computerPoint = 1;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);        
+    }
+
+    return [playerPoint, computerPoint];
+}
+
+function game(){
+    let playerPoints = 0;
+    let computerPoints = 0;
+    let result;
+
+    for(;(playerPoints < 5)&&(computerPoints < 5);)
+    {
+        [playerAdd, computerAdd] = playRound(playerSelection(), computerPlay());
+        playerPoints += playerAdd;
+        computerPoints += computerAdd;
+        console.log(`YOU: ${playerPoints} - CPU: ${computerPoints}`)
+    }
+
+    if (playerPoints > computerPoints)
+    {
+        result = `YOU WIN THE GAME ${playerPoints} - ${computerPoints}`;
+    }
+    else{
+        result = `YOU LOST THE GAME ${playerPoints} - ${computerPoints}`;
+    }
+
+    return result;
+}
+
+console.log(game());
