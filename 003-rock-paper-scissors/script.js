4320464102bcd8662e335bfd39afcfa66de5d5d7
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-03-15 11:41:38 +0100

    Computer Play function

--- 003-rock-paper-scissors/script.js
+++ 003-rock-paper-scissors/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+function computerPlay(){
+    let randomNumber = Math.floor(Math.random()*3);
+    let play;
+
+    switch(randomNumber){
+        case 0:
+            play = "Rock";
+            break;
+        case 1: 
+            play = "Paper";
+            break;
+        case 2:
+            play = "Scissors";
+            break;
+    }
+
+    return play;
+}
+
+console.log(computerPlay());
