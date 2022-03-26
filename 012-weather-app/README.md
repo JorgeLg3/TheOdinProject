b8e255391a33702e9ab0720d75d54cd287dd5144
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-03-26 19:21:33 +0100

    Style the page

---
+++ 012-weather-app/README.md
@@ -0,0 +1,14 @@
+# 12th TOP project: Weather App
+In this new project of "TOP: full stack javascript path" the task is to develop a weather application, which requests to the API the weather in the place requested by the user, and displays all the information in the page. [Project guideline](https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/weather-app).
+
+## Skills
+- HTML
+- CSS
+- Javascript
+    - DOM manipulation
+    - API communication
+    - Async functions
+    - Promises
+
+## Live Preview
+[Weather app](https://jorgelg3.github.io/TheOdinProject/012-weather-app)
--- 012-weather-app/script.js
+++ 012-weather-app/script.js
@@ -1,3 +1,6 @@
+
+displayData(getData('trondheim')); //default value
+
 const searchBtn = document.querySelector('#search-button');
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
@@ -10,8 +13,6 @@ function submitWeather(e){
     const location = input.value;
     displayData(getData(location));
 }
-
-console.log('Hello world!');
 
  //CONVENTIONAL VERSION
 /*function apiRequest(location){
@@ -40,49 +41,84 @@ async function getData(location){
         console.log(error);
     }
 }
-/*
-const processData = (() => {
-    const data = (data) => 
-
-    const place = data.name;
-    const feelTemp = data.main.feels_like;
-    const actualTemp = data.main.temp;
-    const humidity = data.main.humidity;
-    const wind = data.wind.speed;
-
-    return {
-        place,
-        feelTemp,
-        actualTemp,
-        humidity,
-        wind
-    }
-})();*/
 
 function processData(data){
     const place = data.name;
+    const country = data.sys.country;
     const feelTemp = data.main.feels_like;
     const actualTemp = data.main.temp;
     const humidity = data.main.humidity;
     const wind = data.wind.speed;
+    const lat = data.coord.lat;
+    const lon = data.coord.lon;
+    const min = data.main.temp_min;
+    const max = data.main.temp_max;
+    const description = data.weather[0].description;
+    const main = data.weather[0].main;
 
     return {
         place,
         feelTemp,
         actualTemp,
         humidity,
-        wind
+        wind,
+        country,
+        lat,
+        lon,
+        min,
+        max,
+        description,
+        main
     }
 }
 
 function displayData(data){
     data.then(function(response){
-        const card = document.createElement('div');
-        card.textContent = Math.round((response.actualTemp-273.15)*10)/10;
-        const main = document.querySelector('main');
-        main.appendChild(card);
+        const temp = document.querySelector('.temp');
+        temp.textContent = Math.round((response.actualTemp-273.15)*10)/10 + '°C';
+        const min = document.querySelector('.min');
+        min.textContent = Math.round((response.min-273.15)*10)/10 + '°C';
+        const max = document.querySelector('.max');
+        max.textContent = Math.round((response.max-273.15)*10)/10 + '°C';
+        const wind = document.querySelector('.wind');
+        wind.textContent = response.wind + 'km/h';
+        const humidity = document.querySelector('.humidity');
+        humidity.textContent = response.humidity + '%';
+        const description = document.querySelector('.description');
+        description.textContent = response.description;
+        const img = document.querySelector('.img');
+        img.style.cssText = `background-image: url(./weather-icons-png/${getIcon(response.main)})`;
+        const city = document.querySelector('.city');
+        city.textContent = `${response.place}, ${response.country}`;
+        /*const coords = document.querySelector('.coords');
+        coords.textContent = `${response.lat}, ${response.lon}`;*/
     })
 }
-const city = 'murcia';
-//console.log(displayData(getData(city)));
+
+function getIcon(main){
+    let file = '';
+    switch(main){
+        case 'Clouds':
+            file = 'Cloudy.png';
+        break;
+        case 'Clear':
+            file = 'Sunny.png';
+        break;
+        case 'Snow':
+            file = 'OccLightSnow.png';
+        break;
+        case 'Rain':
+            file = 'HeavyRain.png';
+        break;
+        case 'Drizzle':
+            file = 'ModRainSwrsDay.png';
+        break;
+        case 'Thunderstorm':
+            file = 'CloudRainThunder.png';
+        break;
+        default:
+            file = 'Sunny.png';
+    }
+    return file;
+}
 
