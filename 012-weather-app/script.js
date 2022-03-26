d83845a8b24f18022c0531de9c6320225fec41e9
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-03-26 17:01:11 +0100

    Get and process data from API

--- 012-weather-app/script.js
+++ 012-weather-app/script.js
@@ -1,5 +1,75 @@
 console.log('Hello world!');
 
-function apiRequest(location){
-    fetch('')
+ //CONVENTIONAL VERSION
+/*function apiRequest(location){
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=bcc19efb154f39f58361cf61886628fd', {
+        mode: 'cors'
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response) {
+        return response;
+    });
+}*/
+
+// ASYNC VERSION
+
+async function getData(location){
+    try {
+        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=bcc19efb154f39f58361cf61886628fd`, {
+            mode: 'cors'
+        });
+        let catData = await response.json();
+        console.log(catData);
+        return processData(catData);
+    } catch (error){
+        console.log(error);
+    }
 }
+/*
+const processData = (() => {
+    const data = (data) => 
+
+    const place = data.name;
+    const feelTemp = data.main.feels_like;
+    const actualTemp = data.main.temp;
+    const humidity = data.main.humidity;
+    const wind = data.wind.speed;
+
+    return {
+        place,
+        feelTemp,
+        actualTemp,
+        humidity,
+        wind
+    }
+})();*/
+
+function processData(data){
+    const place = data.name;
+    const feelTemp = data.main.feels_like;
+    const actualTemp = data.main.temp;
+    const humidity = data.main.humidity;
+    const wind = data.wind.speed;
+
+    return {
+        place,
+        feelTemp,
+        actualTemp,
+        humidity,
+        wind
+    }
+}
+
+function displayData(data){
+    data.then(function(response){
+        const card = document.createElement('div');
+        card.textContent = Math.round((response.actualTemp-273.15)*10)/10;
+        const main = document.querySelector('main');
+        main.appendChild(card);
+    })
+}
+const city = 'murcia';
+console.log(displayData(getData(city)));
+
