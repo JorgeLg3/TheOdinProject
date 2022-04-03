e75346f752763745e960dc2145abaaacb104e621
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-04-03 19:07:19 +0200

    Initial commit

---
+++ 011-todo-list/src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+//import Icon from './icon.png';
+
+const body = document.querySelector('body');
+const div = document.createElement('div');
+div.textContent = 'HOla';
+body.appendChild(div);
