e72f0c35fab09cea9ff067386a8126045de0bbb8
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-05-10 12:49:46 +0200

    Adding support react routing in gh pages

--- 017-shopping-cart/src/RouteSwitch.js
+++ 017-shopping-cart/src/RouteSwitch.js
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Shop from "./components/Shop";
 
 const RouteSwitch = () => {
   return (
-    <HashRouter basename="/TheOdinProject/017-shopping-cart/build">
+    <BrowserRouter basename="/TheOdinProject/017-shopping-cart/build">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/shop" element={<Shop />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
