857953b3828e4c346b01f0c73cbd22d1ac69d2c8
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-05-11 19:02:17 +0200

    Item main structure

--- 017-shopping-cart/src/RouteSwitch.js
+++ 017-shopping-cart/src/RouteSwitch.js
@@ -1,13 +1,19 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
 import Shop from "./components/Shop";
+import ErrorPage from "./components/ErrorPage";
 
 const RouteSwitch = () => {
   return (
     <HashRouter basename="/">
+        <nav>
+            <Link to="/"> Home </Link>
+            <Link to="/shop"> Shop </Link>
+        </nav>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </HashRouter>
   );
--- 017-shopping-cart/src/components/Shop.js
+++ 017-shopping-cart/src/components/Shop.js
@@ -1,7 +1,13 @@
+import Item from "./Item";
+
+
 const Shop = () => {
     return (
       <div>
         <h1>Hello from Shop</h1>
+        <div>
+            < Item name="Product 1" file="image1.jpg" price="25.88"/>
+        </div>
       </div>
     );
   };
