b4a2a54e6ea9ff4fcdbd54ccac5d5407c1c13fb7
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-05-28 23:32:13 +0200

    IÑnitial commit

---
+++ 020-personal-portfolio/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hey there!.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
