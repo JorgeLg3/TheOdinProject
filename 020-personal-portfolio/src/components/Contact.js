61484e544588a4b5cc7805a37a7be2add4e5ade8
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-07-22 18:20:48 +0200

    Contact design

--- 020-personal-portfolio/src/components/Contact.js
+++ 020-personal-portfolio/src/components/Contact.js
@@ -8,15 +8,21 @@ function Contact() {
             <div className='contact-box'>
                 <h1>Contact me</h1>
                 <p>Please get in touch if you think our work could be mutually beneficial!</p>
-                <p>1234 random Road Random Town, California 12345</p>
-                <div className='contact-field'>
-                    <i class="fa-solid fa-phone"></i>
-                    <span>555-555-5555</span>
+                <div>
+                    <p>1234 Random Road </p>
+                    <p>Random Town, California 12345</p>
                 </div>
-                <div className='contact-field'>
-                    <i class="fa-solid fa-envelope"></i>
-                    <span>ashleywilliams.is.not.real@gmail.com</span>
+                <div>
+                    <div className='contact-field'>
+                        <i class="fa-solid fa-phone"></i>
+                        <span>555-555-5555</span>
+                    </div>
+                    <div className='contact-field'>
+                        <i class="fa-solid fa-envelope"></i>
+                        <span>ashleywilliams.is.not.real@gmail.com</span>
+                    </div>
                 </div>
+                
                 <div className='social'>
                     <i class="fa-brands fa-twitter"></i>
                     <i class="fa-brands fa-linkedin"></i>
